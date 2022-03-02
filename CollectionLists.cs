3addfe35c2bff9fe9ad2fcfb544c1d1a3962d3cc
hasnahaseebkc <hasna.haseeb.k.c@ibm.com>
2022-03-02 16:00:44 +0530

    comit

---
+++ CollectionLists.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class CollectionLists
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            //ArrayList Collection
+            var data = new ArrayList();
+            data.Add("Demo");       //add element
+            data.Add(1);
+            data.Add(5);
+            data.Add(26);
+            data.Add(56.4);
+            data.Add(32);
+            data.Remove(5);         // remove element
+            foreach (object obj in data)
+            {
+                Console.WriteLine(obj);
+            }
+
+
+            var value = new List<string>();     //list collection
+            value.Add("Cricket");
+            value.Add("Football");
+            value.Add("Volleyball");
+            value.Add("Hockey");
+            value.Add("Basketball");
+            value.Add("Tennis");
+            value.Remove("Football");
+            value.Remove("Tennis");
+            value.Insert(3, "Badminton");
+            foreach( string st in value)
+            {
+                Console.WriteLine(st);
+            }
+
+
+            var val = new SortedList<string, int>();
+            val.Add("java", 3);
+            val.Add("javascript", 4);
+            val.Add("c-sharp", 5);
+            val.Add("dotnet", 25);
+
+            foreach( object ob in val)
+            {
+                Console.WriteLine(ob);
+            }
+
+
+            var link = new LinkedList<int>();
+            link.AddLast(13);
+            link.AddLast(33);
+            link.AddLast(23);
+            link.AddLast(51);
+            link.AddLast(60);
+            link.AddFirst(4);
+            link.AddFirst(6);
+            LinkedListNode<int> node = link.Find(51);
+            link.AddBefore(node, 40);
+            foreach( int i in link)
+                Console.WriteLine(i);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ jaggedarrays2d.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class jaggedarrays2d
+    {
+        static void Main(string[] args)
+        {
+
+            //create a jagged array
+            int[][] jaggedArray =
+            {
+                new int[] { 1, 3, 5 },
+                new int[] {2, 4 }
+            };
+
+
+            //print elements of jagged array
+            Console.WriteLine("jaggedArray[1][0]: " + jaggedArray[1][0]);   //2
+            Console.WriteLine("jaggedArray[1][1]: " + jaggedArray[1][1]);   //4
+            Console.WriteLine("jaggedArray[1][0]: " + jaggedArray[0][2]);   //5
+
+
+            //declare and initialise jagged array with 2D array
+
+            int[][,] jaggedArray2d = new int[3][,]
+            {
+                new int[,] { { 1, 8 }, { 6, 7 } },
+                new int[,] { { 0, 3 }, { 5, 6 },{ 9, 10 } },
+                new int[,] { { 11, 23 }, {100,88 },{0,10 } }
+
+            };
+
+            Console.WriteLine(jaggedArray2d[0][0, 1]);
+            Console.WriteLine(jaggedArray2d[1][2, 1]);
+            Console.WriteLine(jaggedArray2d[2][1, 0]);
+
+            Console.ReadLine();
+        }
+    }
+}
